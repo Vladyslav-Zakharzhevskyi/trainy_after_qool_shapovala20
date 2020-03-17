fb403f973fb99883a47c85cdf4aab6cb645fe0b8
Vlad <Vladyslav@qooling.com>
2020-03-17 10:15:02 +0200

    add image

--- angularclient/src/app/app.module.ts
+++ angularclient/src/app/app.module.ts
@@ -7,18 +7,22 @@ import { AppComponent } from './app.component';
 import { PersonsComponent } from './ui/persons/persons.component';
 import { AppMaterialModule} from './resourceconfig/angular-material-module';
 import { NavHeaderComponent } from './ui/nav-header/nav-header.component';
+import { StartComponent } from './ui/start/start.component';
+import {MatMenuModule} from "@angular/material/menu";
 
 @NgModule({
   declarations: [
     AppComponent,
     PersonsComponent,
-    NavHeaderComponent
+    NavHeaderComponent,
+    StartComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    AppMaterialModule
+    AppMaterialModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
